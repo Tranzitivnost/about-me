9e61373a0d73aaeef0b71b91e8f6874ac71956e9
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-08-05 22:56:50 +0200

    chore: Add initial project files and configurations

---
+++ app/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Here will be your amazing app</header>
+    </div>
+  );
+};
+
+export default App;
---
+++ app/src/store.ts
@@ -0,0 +1,27 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+
+const rootReducer = combineSlices(); // TODO add slices here
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export const makeStore = (preloadedState?: Partial<RootState>) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  })
+  setupListeners(store.dispatch)
+  return store
+}
+
+export const store = makeStore()
+
+export type AppStore = typeof store
+export type AppDispatch = AppStore["dispatch"]
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
