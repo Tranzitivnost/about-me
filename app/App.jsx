a31c6e334bf7b615b85e5fd3b120fb7e1ef36a07
tranzitivnost <alevtinapatkina@gmail.com>
2024-08-06 18:40:27 +0200

    add initial layout, buttons, style

---
+++ app/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SocialMediaButton from "./src/components/SocialMediaButton.jsx";
+
+const App = () => {
+  return (
+    <div className="App">
+      <SocialMediaButton />
+    </div>
+  );
+};
+
+export default App;
---
+++ app/src/components/SocialMediaButton.jsx
@@ -0,0 +1,18 @@
+import "./SocialMediaButton.css";
+
+const SocialMediaButton = () => {
+  return (
+    <div class="blackCard">
+      <button name="button" class="black">
+        GitHub
+      </button>
+      <button name="button" class="black">
+        Linkedln
+      </button>
+      <button name="button" class="black">
+        Instagram
+      </button>
+    </div>
+  );
+};
+export default SocialMediaButton;
