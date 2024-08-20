f9119d79d670012d945f68af0b9ddfd1539fab1b
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-08-20 23:42:53 +0200

    add component avatar

--- app/src/components/Avatar.jsx
+++ app/src/components/Avatar.jsx
@@ -1,10 +1,10 @@
 import "./Avatar.css";
 // import myImage from "./images/1_avatar.png";
 
-const Avatar = ({ src, imageSize = 130 }) => {
+const Avatar = ({ src, size = "m" }) => {
   return (
-    <div class="photo_container">
-      <img class="photoAvatar" src={src} alt="photo-1" width={imageSize} />
+    <div className="photo_container">
+      <img className={`photoAvatar img-${size}`} src={src} alt="photo-1" />
     </div>
   );
 };
--- app/src/components/InformationAboutMe.jsx
+++ app/src/components/InformationAboutMe.jsx
@@ -1,12 +1,11 @@
+import Avatar from "./Avatar";
 import "./InformationAboutMe.css";
 import myImage from "./images/1_avatar.png";
 
 const InformationAboutMe = () => {
   return (
     <div class="header">
-      <div class="photo_container">
-        <img class="photoAvatar" src={myImage} alt="photo-1" />
-      </div>
+      <Avatar src={myImage} size="m" />
       <div class="article-section">
         <h2 class="name">Valentina Savitskaia</h2>
         <h2>Serbia, Belgrade</h2>
