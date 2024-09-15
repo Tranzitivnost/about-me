6d7fd3c0450596831586e6d614b768c690889d59
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-09-15 19:09:16 +0200

    adds module styles for App.jsx

--- app/App.jsx
+++ app/App.jsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import { useLayoutEffect } from "react";
+import styles from "./App.module.css";
 import Card from "./src/components/Card.jsx";
 import Container from "./src/components/Container.jsx";
+import clsx from "clsx";
 
 const App = () => {
   return (
-    <Container className="App">
+    <Container className={clsx([styles.app])}>
       <Card />
     </Container>
   );
