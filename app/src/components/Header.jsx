81864e002629b2aed27acf4c90025de1eef420a0
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-09-16 18:40:09 +0200

    fixes header moduled styles

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -2,7 +2,10 @@ import styles from "./Header.module.css";
 import clsx from "clsx";
 
 const Header = ({ size = "xl", color = "white", children }) => {
-  const headerStyles = clsx(styles[`color-${color}`], styles[`text-${size}`]);
+  const headerStyles = clsx(
+    styles[`color-${color}`],
+    styles[(`text-${size}`, styles.h2)],
+  );
   return <h2 className={headerStyles}>{children}</h2>;
 };
 
